b460239c4d32c32516ac6b1600f307ec530fe454
Yury Silvestrov-Henocq <35099962+yursilv@users.noreply.github.com>
2018-05-30 10:08:39 +0200

    Create Corneille.ino

---
+++ Code/Arduino/Corneille/Corneille.ino
@@ -0,0 +1,36 @@
+/*
+  Corneille le réveil est un projet Arduino 
+  dont le but est la création d'un réveil qui 
+  non seulement produit des bruits désagréables 
+  mais qui en plus roule sans arrêt pour nous 
+  empêcher de l'éteindre.
+
+  Branchements pour l'accéléromètre:
+  PC0 - Xout
+  PC1 - Yout
+  PC2 - Zout
+
+  Branchements pour le module horloge:
+  5V - 5V/VCC
+  GND - GND
+  PC4 - SDA
+  PC5 - SCL
+
+  Branchements pour les moteurs:
+  PD5 - sorite PWM 1 (celle du premier moteur)
+  PD6 - sorite PWM 2 (celle du deuxième moteur)
+
+  Branchements pour le module BT:
+  5V - VCC
+  GND - GND
+  PB2 - TX
+  PB3 - RX
+
+  Pour savoir si Corneille est sur son socle:
+  PD1 - fil du chargement de la batterie
+
+  Buzzer:
+  5V - VCC
+  PD7 - I/O
+  GND - GND
+*/
